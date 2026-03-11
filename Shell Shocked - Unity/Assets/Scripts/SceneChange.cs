cf7166a3cfed36bef109272f4dfa4ec6a25c7157
Maddison2007 <1804106@fife.ac.uk>
2026-03-11 12:34:41 +0000

    created a working button and built it to the android tablet

---
+++ Shell Shocked - Unity/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+    public void ChangeToScene (string targetSceneName)
+    {
+        SceneManager.LoadScene(targetSceneName);
+
+    }
+    
+
+}
