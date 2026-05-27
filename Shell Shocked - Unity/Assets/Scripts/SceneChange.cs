bb3890d8450d45c3825ce4031b8c7559f771a372
Maddison2007 <1804106@fife.ac.uk>
2026-05-27 13:57:28 +0100

    fixed the bar as it was offset

--- Shell Shocked - Unity/Assets/Scripts/SceneChange.cs
+++ Shell Shocked - Unity/Assets/Scripts/SceneChange.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+//This allows the scene to change when the play button is pressed
 public class SceneChange : MonoBehaviour
 {
-
+    
     public void ChangeToScene (string targetSceneName)
     {
         SceneManager.LoadScene(targetSceneName);
